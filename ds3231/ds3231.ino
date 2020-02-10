f746c98105799f72471cdecbbdabbc3c99d5a25e
LitningWolf <60525399+LitningWolf@users.noreply.github.com>
2020-02-09 22:01:30 -0600

    working code for clock
    lights are working for 1-4min 5-55min and 1-12hour.
    no calander yet

--- ds3231/ds3231.ino
+++ ds3231/ds3231.ino
@@ -4,6 +4,8 @@
 
 RTC_DS3231 rtc;
 Adafruit_MCP23017 mcp1;
+Adafruit_MCP23017 mcp2;
+
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -26,6 +28,19 @@ mcp1.pinMode(12, OUTPUT);//45
 mcp1.pinMode(13, OUTPUT);//50
 mcp1.pinMode(14, OUTPUT);//55
 
+mcp2.begin(001);
+mcp2.pinMode(0, OUTPUT);//1
+mcp2.pinMode(1, OUTPUT);//2
+mcp2.pinMode(2, OUTPUT);//3
+mcp2.pinMode(3, OUTPUT);//4
+mcp2.pinMode(4, OUTPUT);//5
+mcp2.pinMode(5, OUTPUT);//6
+mcp2.pinMode(6, OUTPUT);//7
+mcp2.pinMode(7, OUTPUT);//8
+mcp2.pinMode(8, OUTPUT);//9
+mcp2.pinMode(9, OUTPUT);//10
+mcp2.pinMode(10, OUTPUT);//11
+mcp2.pinMode(11, OUTPUT);//12
 
 //light
 
@@ -84,69 +99,129 @@ if (now.minute() % 5 == 3)
   mcp1.digitalWrite(2, HIGH);
 if (now.minute() % 5 == 4)
   mcp1.digitalWrite(3, HIGH);
+  
 //leds 5-55
-if (now.minute() % 5 == 5)
+if (now.minute() == 5)
   mcp1.digitalWrite(4, HIGH);
-if (now.minute() % 5 == 10)
+if (now.minute() == 10)
   mcp1.digitalWrite(5, HIGH);
-if (now.minute() % 5 == 15)
+if (now.minute() == 15)
   mcp1.digitalWrite(6, HIGH);
-if (now.minute() % 5 == 20)
+if (now.minute() == 20)
   mcp1.digitalWrite(7, HIGH);
-if (now.minute() % 5 == 25)
+if (now.minute() == 25)
   mcp1.digitalWrite(8, HIGH);
-if (now.minute() % 5 == 30)
+if (now.minute() == 30)
   mcp1.digitalWrite(9, HIGH);
-if (now.minute() % 5 == 35)
+if (now.minute() == 35)
   mcp1.digitalWrite(10, HIGH);
-if (now.minute() % 5 == 40)
+if (now.minute() == 40)
   mcp1.digitalWrite(11, HIGH);
-if (now.minute() % 5 == 45)
+if (now.minute() == 45)
   mcp1.digitalWrite(12, HIGH);
-if (now.minute() % 5 == 50)
+if (now.minute() == 50)
   mcp1.digitalWrite(13, HIGH);
-if (now.minute() % 5 == 55)
+if (now.minute() == 55)
   mcp1.digitalWrite(14, HIGH);
+  
+// leds 1-12 hours
+if (now.hour() == 1)
+  mcp2.digitalWrite(0, HIGH);
+if (now.hour() == 2)
+  mcp2.digitalWrite(1, HIGH);
+if (now.hour() == 3)
+  mcp2.digitalWrite(2, HIGH);
+if (now.hour() == 4)
+  mcp2.digitalWrite(3, HIGH);
+if (now.hour() == 5)
+  mcp2.digitalWrite(4, HIGH);
+if (now.hour() == 6)
+  mcp2.digitalWrite(5, HIGH);
+if (now.hour() == 7)
+  mcp2.digitalWrite(6, HIGH);
+if (now.hour() == 8)
+  mcp2.digitalWrite(7, HIGH);
+if (now.hour() == 9)
+  mcp2.digitalWrite(8, HIGH);
+if (now.hour() == 10)
+  mcp2.digitalWrite(9, HIGH);
+if (now.hour() == 11)
+  mcp2.digitalWrite(10, HIGH);
+if (now.hour() == 12)
+  mcp2.digitalWrite(11, HIGH);
+if (now.hour() == 13)
+  mcp2.digitalWrite(0, HIGH);
+if (now.hour() == 14)
+  mcp2.digitalWrite(1, HIGH);
+if (now.hour() == 15)
+  mcp2.digitalWrite(2, HIGH);
+if (now.hour() == 16)
+  mcp2.digitalWrite(3, HIGH);
+if (now.hour() == 17)
+  mcp2.digitalWrite(4, HIGH);
+if (now.hour() == 18)
+  mcp2.digitalWrite(5, HIGH);
+if (now.hour() == 19)
+  mcp2.digitalWrite(6, HIGH);
+if (now.hour() == 20)
+  mcp2.digitalWrite(7, HIGH);
+if (now.hour() == 21)
+  mcp2.digitalWrite(8, HIGH);
+if (now.hour() == 22)
+  mcp2.digitalWrite(9, HIGH);
+if (now.hour() == 23)
+  mcp2.digitalWrite(10, HIGH);
+if (now.hour() == 0)
+  mcp2.digitalWrite(11, HIGH);
+
 
 //off
 // leds 1-4
-if (now.minute() % 10 == 0)
+if (now.minute() % 5 == 0){
   mcp1.digitalWrite(0, LOW);
-if (now.minute() % 10 == 0)
   mcp1.digitalWrite(1, LOW);
-if (now.minute() % 10 == 0)
   mcp1.digitalWrite(2, LOW);
-if (now.minute() % 10 == 0)
   mcp1.digitalWrite(3, LOW);
+}
 // leds 5-55
-if (now.minute() % 10 == 5)
-  mcp1.digitalWrite(0, LOW);
-if (now.minute() % 10 == 5)
-  mcp1.digitalWrite(1, LOW);
-if (now.minute() % 10 == 5)
-  mcp1.digitalWrite(2, LOW);
-if (now.minute() % 10 == 5)
-  mcp1.digitalWrite(3, LOW);
-if (now.minute() % 5 == 0)
+if (now.minute() / 5 == 0){
   mcp1.digitalWrite(4, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(5, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(6, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(7, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(8, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(9, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(10, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(11, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(12, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(13, LOW);
-if (now.minute() % 5 == 0)
   mcp1.digitalWrite(14, LOW);
+}
+// leds 1-12
+if (now.hour() == 1){
+  mcp2.digitalWrite(1, LOW);
+  mcp2.digitalWrite(2, LOW);
+  mcp2.digitalWrite(3, LOW);
+  mcp2.digitalWrite(4, LOW);
+  mcp2.digitalWrite(5, LOW);
+  mcp2.digitalWrite(6, LOW);
+  mcp2.digitalWrite(7, LOW);
+  mcp2.digitalWrite(8, LOW);
+  mcp2.digitalWrite(9, LOW);
+  mcp2.digitalWrite(10, LOW);
+  mcp2.digitalWrite(11, LOW);
+}
+if (now.hour() == 13){
+  mcp2.digitalWrite(1, LOW);
+  mcp2.digitalWrite(2, LOW);
+  mcp2.digitalWrite(3, LOW);
+  mcp2.digitalWrite(4, LOW);
+  mcp2.digitalWrite(5, LOW);
+  mcp2.digitalWrite(6, LOW);
+  mcp2.digitalWrite(7, LOW);
+  mcp2.digitalWrite(8, LOW);
+  mcp2.digitalWrite(9, LOW);
+  mcp2.digitalWrite(10, LOW);
+  mcp2.digitalWrite(11, LOW);
+}
 }
