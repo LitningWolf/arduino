a6508dda0d0a5ec5497ee32335e28db896254b73
LitningWolf <60525399+LitningWolf@users.noreply.github.com>
2020-03-18 16:38:12 -0500

    Updated
    Just Clock hour/min script no dates

--- ds3231/ds3231.ino
+++ ds3231/ds3231.ino
@@ -93,86 +93,279 @@ void loop () {
 // leds 1-4
 if (now.minute() % 5 == 1)
   mcp1.digitalWrite(0, HIGH);
-if (now.minute() % 5 == 2)
+if (now.minute() % 5 == 2){
+  mcp1.digitalWrite(0, HIGH);
+  mcp1.digitalWrite(1, HIGH);}
+if (now.minute() % 5 == 3){
+  mcp1.digitalWrite(0, HIGH);
   mcp1.digitalWrite(1, HIGH);
-if (now.minute() % 5 == 3)
+  mcp1.digitalWrite(2, HIGH);}
+if (now.minute() % 5 == 4){
+  mcp1.digitalWrite(0, HIGH);
+  mcp1.digitalWrite(1, HIGH);
   mcp1.digitalWrite(2, HIGH);
-if (now.minute() % 5 == 4)
-  mcp1.digitalWrite(3, HIGH);
+  mcp1.digitalWrite(3, HIGH);}
   
 //leds 5-55
 if (now.minute() == 5)
   mcp1.digitalWrite(4, HIGH);
-if (now.minute() == 10)
+if (now.minute() == 10){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);}
+if (now.minute() == 15){
+  mcp1.digitalWrite(4, HIGH);
   mcp1.digitalWrite(5, HIGH);
-if (now.minute() == 15)
+  mcp1.digitalWrite(6, HIGH);}
+if (now.minute() == 20){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
   mcp1.digitalWrite(6, HIGH);
-if (now.minute() == 20)
+  mcp1.digitalWrite(7, HIGH);}
+if (now.minute() == 25){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
   mcp1.digitalWrite(7, HIGH);
-if (now.minute() == 25)
+  mcp1.digitalWrite(8, HIGH);}
+if (now.minute() == 30){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
   mcp1.digitalWrite(8, HIGH);
-if (now.minute() == 30)
+  mcp1.digitalWrite(9, HIGH);}
+if (now.minute() == 35){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
+  mcp1.digitalWrite(8, HIGH);
   mcp1.digitalWrite(9, HIGH);
-if (now.minute() == 35)
+  mcp1.digitalWrite(10, HIGH);}
+if (now.minute() == 40){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
+  mcp1.digitalWrite(8, HIGH);
+  mcp1.digitalWrite(9, HIGH);
   mcp1.digitalWrite(10, HIGH);
-if (now.minute() == 40)
+  mcp1.digitalWrite(11, HIGH);}
+if (now.minute() == 45){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
+  mcp1.digitalWrite(8, HIGH);
+  mcp1.digitalWrite(9, HIGH);
+  mcp1.digitalWrite(10, HIGH);
   mcp1.digitalWrite(11, HIGH);
-if (now.minute() == 45)
+  mcp1.digitalWrite(12, HIGH);}
+if (now.minute() == 50){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
+  mcp1.digitalWrite(8, HIGH);
+  mcp1.digitalWrite(9, HIGH);
+  mcp1.digitalWrite(10, HIGH);
+  mcp1.digitalWrite(11, HIGH);
   mcp1.digitalWrite(12, HIGH);
-if (now.minute() == 50)
+  mcp1.digitalWrite(13, HIGH);}
+if (now.minute() == 55){
+  mcp1.digitalWrite(4, HIGH);
+  mcp1.digitalWrite(5, HIGH);
+  mcp1.digitalWrite(6, HIGH);
+  mcp1.digitalWrite(7, HIGH);
+  mcp1.digitalWrite(8, HIGH);
+  mcp1.digitalWrite(9, HIGH);
+  mcp1.digitalWrite(10, HIGH);
+  mcp1.digitalWrite(11, HIGH);
+  mcp1.digitalWrite(12, HIGH);
   mcp1.digitalWrite(13, HIGH);
-if (now.minute() == 55)
-  mcp1.digitalWrite(14, HIGH);
+  mcp1.digitalWrite(14, HIGH);}
   
 // leds 1-12 hours
 if (now.hour() == 1)
   mcp2.digitalWrite(0, HIGH);
-if (now.hour() == 2)
+if (now.hour() == 2){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);}
+if (now.hour() == 3){
+  mcp2.digitalWrite(0, HIGH);
   mcp2.digitalWrite(1, HIGH);
-if (now.hour() == 3)
+  mcp2.digitalWrite(2, HIGH);}
+if (now.hour() == 4){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
   mcp2.digitalWrite(2, HIGH);
-if (now.hour() == 4)
+  mcp2.digitalWrite(3, HIGH);}
+if (now.hour() == 5){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
   mcp2.digitalWrite(3, HIGH);
-if (now.hour() == 5)
+  mcp2.digitalWrite(4, HIGH);}
+if (now.hour() == 6){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
   mcp2.digitalWrite(4, HIGH);
-if (now.hour() == 6)
+  mcp2.digitalWrite(5, HIGH);}
+if (now.hour() == 7){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
   mcp2.digitalWrite(5, HIGH);
-if (now.hour() == 7)
+  mcp2.digitalWrite(6, HIGH);}
+if (now.hour() == 8){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
   mcp2.digitalWrite(6, HIGH);
-if (now.hour() == 8)
+  mcp2.digitalWrite(7, HIGH);}
+if (now.hour() == 9){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
   mcp2.digitalWrite(7, HIGH);
-if (now.hour() == 9)
+  mcp2.digitalWrite(8, HIGH);}
+if (now.hour() == 10){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
   mcp2.digitalWrite(8, HIGH);
-if (now.hour() == 10)
+  mcp2.digitalWrite(9, HIGH);}
+if (now.hour() == 11){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
+  mcp2.digitalWrite(8, HIGH);
   mcp2.digitalWrite(9, HIGH);
-if (now.hour() == 11)
+  mcp2.digitalWrite(10, HIGH);}
+if (now.hour() == 12){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
+  mcp2.digitalWrite(8, HIGH);
+  mcp2.digitalWrite(9, HIGH);
   mcp2.digitalWrite(10, HIGH);
-if (now.hour() == 12)
-  mcp2.digitalWrite(11, HIGH);
+  mcp2.digitalWrite(11, HIGH);}
 if (now.hour() == 13)
   mcp2.digitalWrite(0, HIGH);
-if (now.hour() == 14)
+if (now.hour() == 14){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);}
+if (now.hour() == 15){
+  mcp2.digitalWrite(0, HIGH);
   mcp2.digitalWrite(1, HIGH);
-if (now.hour() == 15)
+  mcp2.digitalWrite(2, HIGH);}
+if (now.hour() == 16){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
   mcp2.digitalWrite(2, HIGH);
-if (now.hour() == 16)
+  mcp2.digitalWrite(3, HIGH);}
+if (now.hour() == 17){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
   mcp2.digitalWrite(3, HIGH);
-if (now.hour() == 17)
+  mcp2.digitalWrite(4, HIGH);}
+if (now.hour() == 18){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
   mcp2.digitalWrite(4, HIGH);
-if (now.hour() == 18)
+  mcp2.digitalWrite(5, HIGH);}
+if (now.hour() == 19){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
   mcp2.digitalWrite(5, HIGH);
-if (now.hour() == 19)
+  mcp2.digitalWrite(6, HIGH);}
+if (now.hour() == 20){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
   mcp2.digitalWrite(6, HIGH);
-if (now.hour() == 20)
+  mcp2.digitalWrite(7, HIGH);}
+if (now.hour() == 21){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
   mcp2.digitalWrite(7, HIGH);
-if (now.hour() == 21)
+  mcp2.digitalWrite(8, HIGH);}
+if (now.hour() == 22){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
   mcp2.digitalWrite(8, HIGH);
-if (now.hour() == 22)
+  mcp2.digitalWrite(9, HIGH);}
+if (now.hour() == 23){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
+  mcp2.digitalWrite(8, HIGH);
   mcp2.digitalWrite(9, HIGH);
-if (now.hour() == 23)
+  mcp2.digitalWrite(10, HIGH);}
+if (now.hour() == 0){
+  mcp2.digitalWrite(0, HIGH);
+  mcp2.digitalWrite(1, HIGH);
+  mcp2.digitalWrite(2, HIGH);
+  mcp2.digitalWrite(3, HIGH);
+  mcp2.digitalWrite(4, HIGH);
+  mcp2.digitalWrite(5, HIGH);
+  mcp2.digitalWrite(6, HIGH);
+  mcp2.digitalWrite(7, HIGH);
+  mcp2.digitalWrite(8, HIGH);
+  mcp2.digitalWrite(9, HIGH);
   mcp2.digitalWrite(10, HIGH);
-if (now.hour() == 0)
-  mcp2.digitalWrite(11, HIGH);
+  mcp2.digitalWrite(11, HIGH);}
 
 
 //off
